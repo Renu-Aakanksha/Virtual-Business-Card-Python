c645ed4716caaf661e10f8992d48c5cf7c5e7716
Renu Aakanksha Veesam <60746640+Renu-Aakanksha@users.noreply.github.com>
2024-07-12 18:39:07 -0500

    Delete vcard-master directory

--- vcard-master/donate/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('donate/', views.donate, name='donate'),
-    path('payment-status/', views.payment_status, name='payment-status'),
-]
